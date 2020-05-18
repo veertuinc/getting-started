a166caef8b2f8af2632f0c8c6e80fd5a1a2f10a5
Nathan Pierce <connarpierce@gmail.com>
2020-05-18 13:23:05 -0400

    jenkins fixes

--- JENKINS/create-jenkins-docker.bash
+++ JENKINS/create-jenkins-docker.bash
@@ -1,14 +1,12 @@
 #!/bin/bash
-set -exo pipefail
+set -eo pipefail
 . ../shared.bash # source in the ports
-SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
-cd $SCRIPT_DIR
-docker-compose down &>/dev/null || true
+SERVICE_PORT="8080"
+CUR_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
+docker-compose down || true
 rm -rf $JENKINS_DATA_DIR
+cd $CUR_DIR
 mkdir -p $JENKINS_DATA_DIR
-modify_hosts $JENKINS_DOCKER_CONTAINER_NAME
-# Container start
-echo "]] Creating Jenkins Container"
 cp -f log.properties $JENKINS_DATA_DIR # Enable debug logging
 cat > docker-compose.yml <<BLOCK
 version: '3.7'
@@ -18,20 +16,18 @@ services:
     image: jenkins/jenkins:$JENKINS_TAG_VERSION
     restart: always
     ports:
-      - "$JENKINS_PORT:$JENKINS_SERVICE_PORT"
+      - "$JENKINS_PORT:$SERVICE_PORT"
       - "50000:50000"
     volumes:
       - $JENKINS_DATA_DIR:/var/jenkins_home
     environment:
-      # - JENKINS_OPTS=" --httpPort=-1 --httpsPort=$JENKINS_SERVICE_PORT "
+      # - JENKINS_OPTS=" --httpPort=-1 --httpsPort=$SERVICE_PORT "
       - JAVA_OPTS="-Djava.util.logging.config.file=/var/jenkins_home/log.properties"
 BLOCK
 docker-compose up -d
-echo "Waiting for Jenkins to start properly..."
-while [[ $(docker logs --tail 1 $JENKINS_DOCKER_CONTAINER_NAME 2>&1) != "FINE: Effective SlaveRestarter on : null" ]]; do
-  sleep 10
-done
-echo "]] Configuring Jenkins"
+modify_hosts $JENKINS_DOCKER_CONTAINER_NAME
+echo "Waiting 60 seconds for Jenkins to start properly..."
+sleep 60
 # Disable the "Unlock Jenkins" (initialAdminPassword) 
 sed -i '' 's/NEW/RUNNING/' $JENKINS_DATA_DIR/config.xml
 [[ -z $(grep "<installStateName>RUNNING</installStateName>" $JENKINS_DATA_DIR/config.xml) ]] && echo "sed didn't work" && exit 1
@@ -47,8 +43,8 @@ cat > $JENKINS_DATA_DIR/jenkins.model.JenkinsLocationConfiguration.xml <<BLOCK
 BLOCK
 docker-compose restart
 # Plugins
-echo "]] Installing Plugins (may take a while)"
-sleep 20
+echo "Installing Plugins (may take a while)..."
+sleep 80 # Waits for "jenkins.slaves.restarter.JnlpSlaveRestarterInstaller install" to finish
 jenkins_plugin_install "github@$GITHUB_PLUGIN_VERSION"
 jenkins_plugin_install "anka-build@$ANKA_PLUGIN_VERSION"
 jenkins_plugin_install "pipeline-model-definition@$PIPELINE_PLUGIN_VERSION"
@@ -66,11 +62,9 @@ curl -X POST -H "$CRUMB" --cookie "$COOKIEJAR" http://$JENKINS_DOCKER_CONTAINER_
   }
 }'
 # Clone the jobs examples
-echo "]] Installing example jobs"
 git clone https://github.com/veertuinc/jenkins-job-examples.git $JENKINS_DATA_DIR/jobs
 docker-compose restart
 #
-set +x
 echo "================================================================================="
 echo "Jenkins is now accessible at: http://$JENKINS_DOCKER_CONTAINER_NAME:$JENKINS_PORT
 Documentation: https://ankadocs.veertu.com/docs/anka-build-cloud/ci-plugins/jenkins

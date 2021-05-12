4ed23f614d85f9689715a692632099daaae83f6e
Nathan Pierce <connarpierce@gmail.com>
2021-05-12 11:12:14 -0400

    quick fix

--- GITLAB/install-and-run-anka-gitlab-runners-on-docker.bash
+++ GITLAB/install-and-run-anka-gitlab-runners-on-docker.bash
@@ -20,12 +20,12 @@ if [[ $1 != "--uninstall" ]]; then
   SHARED_REGISTRATION_TOKEN="$(docker exec -i $GITLAB_DOCKER_CONTAINER_NAME bash -c "gitlab-rails runner -e production \"puts Gitlab::CurrentSettings.current_application_settings.runners_registration_token\"")"
   echo "]] Starting a shared Anka GitLab Runner (Docker container: $GITLAB_RUNNER_SHARED_RUNNER_NAME) and connecting it to your GitLab"
   docker run --name $GITLAB_RUNNER_SHARED_RUNNER_NAME $VOLUMES -ti -d veertu/anka-gitlab-runner-amd64:v$GITLAB_ANKA_RUNNER_VERSION \
-  --url "http://host.docker.internal:$GITLAB_PORT" \
+  --url "http://172.17.0.1:$GITLAB_PORT" \
   --registration-token $SHARED_REGISTRATION_TOKEN \
   --ssh-user $ANKA_VM_USER \
   --ssh-password $ANKA_VM_PASSWORD \
   --name "localhost shared runner" \
-  --anka-controller-address "${URL_PROTOCOL}host.docker.internal:$CLOUD_CONTROLLER_PORT" \
+  --anka-controller-address "${URL_PROTOCOL}172.17.0.1:$CLOUD_CONTROLLER_PORT" \
   --anka-template-uuid $GITLAB_RUNNER_VM_TEMPLATE_UUID \
   --anka-tag $GITLAB_ANKA_VM_TEMPLATE_TAG \
   --executor anka \
@@ -36,12 +36,12 @@ if [[ $1 != "--uninstall" ]]; then
   PROJECT_REGISTRATION_TOKEN=$(docker exec -i $GITLAB_DOCKER_CONTAINER_NAME bash -c "gitlab-rails runner -e production \"puts Project.find_by_id($GITLAB_EXAMPLE_PROJECT_ID).runners_token\"")
   echo "]] Starting a project specific Anka GitLab Runner (Docker container: $GITLAB_RUNNER_PROJECT_RUNNER_NAME) and connecting it to your GitLab"
   docker run --name $GITLAB_RUNNER_PROJECT_RUNNER_NAME $VOLUMES -ti -d veertu/anka-gitlab-runner-amd64:v$GITLAB_ANKA_RUNNER_VERSION \
-  --url "http://host.docker.internal:$GITLAB_PORT" \
+  --url "http://172.17.0.1:$GITLAB_PORT" \
   --registration-token $PROJECT_REGISTRATION_TOKEN \
   --ssh-user $ANKA_VM_USER \
   --ssh-password $ANKA_VM_PASSWORD \
   --name "localhost project specific runner" \
-  --anka-controller-address "${URL_PROTOCOL}host.docker.internal:$CLOUD_CONTROLLER_PORT" \
+  --anka-controller-address "${URL_PROTOCOL}172.17.0.1:$CLOUD_CONTROLLER_PORT" \
   --anka-template-uuid $GITLAB_RUNNER_VM_TEMPLATE_UUID \
   --anka-tag $GITLAB_ANKA_VM_TEMPLATE_TAG \
   --executor anka \

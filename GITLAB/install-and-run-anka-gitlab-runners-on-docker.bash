9d687b0608a73d8c215949dee3caef0df9d03397
Nathan Pierce <connarpierce@gmail.com>
2021-10-15 16:30:58 -0400

    DOCKER RUN EXTRAS

--- GITLAB/install-and-run-anka-gitlab-runners-on-docker.bash
+++ GITLAB/install-and-run-anka-gitlab-runners-on-docker.bash
@@ -18,11 +18,11 @@ if [[ $1 != "--uninstall" ]]; then
   GITLAB_EXAMPLE_PROJECT_ID=$(curl -s --request GET -H "PRIVATE-TOKEN: $GITLAB_ACCESS_TOKEN" "http://$GITLAB_DOCKER_CONTAINER_NAME:$GITLAB_PORT/api/v4/projects" | jq -r ".[] | select(.name==\"$GITLAB_EXAMPLE_PROJECT_NAME\") | .id")
   # GitLab Runner
   DOCKER_RUN_EXTRAS="--rm -ti -d ${VOLUMES} veertu/anka-gitlab-runner-amd64:v${GITLAB_ANKA_RUNNER_VERSION}"
-  [[ "$(uname)" == "Linux" ]] && DOCKER_RUN_EXTRAS="--add-host=\"host.docker.internal:host-gateway\" ${DOCKER_RUN_EXTRAS}"
+  [[ "$(uname)" == "Linux" ]] && DOCKER_RUN_EXTRAS="--add-host=host.docker.internal:host-gateway ${DOCKER_RUN_EXTRAS}"
   ## Collect the Shared runner token
   SHARED_REGISTRATION_TOKEN="${SHARED_REGISTRATION_TOKEN:-"$(docker exec -i $GITLAB_DOCKER_CONTAINER_NAME bash -c "gitlab-rails runner -e production \"puts Gitlab::CurrentSettings.current_application_settings.runners_registration_token\"")"}"
   echo "]] Starting a shared Anka GitLab Runner (Docker container: $GITLAB_RUNNER_SHARED_RUNNER_NAME) and connecting it to your GitLab"
-  eval docker run ${DOCKER_RUN_EXTRAS} --name $GITLAB_RUNNER_SHARED_RUNNER_NAME \
+  eval docker run --name $GITLAB_RUNNER_SHARED_RUNNER_NAME ${DOCKER_RUN_EXTRAS} \
   --url "http://${DOCKER_HOST_ADDRESS}:$GITLAB_PORT" \
   --registration-token $SHARED_REGISTRATION_TOKEN \
   --ssh-user $ANKA_VM_USER \
@@ -38,7 +38,7 @@ if [[ $1 != "--uninstall" ]]; then
   ## Collect the project runner token
   PROJECT_REGISTRATION_TOKEN=${PROJECT_REGISTRATION_TOKEN:-"$(docker exec -i $GITLAB_DOCKER_CONTAINER_NAME bash -c "gitlab-rails runner -e production \"puts Project.find_by_id($GITLAB_EXAMPLE_PROJECT_ID).runners_token\"")"}
   echo "]] Starting a project specific Anka GitLab Runner (Docker container: $GITLAB_RUNNER_PROJECT_RUNNER_NAME) and connecting it to your GitLab"
-  eval docker run ${DOCKER_RUN_EXTRAS} --name $GITLAB_RUNNER_PROJECT_RUNNER_NAME \
+  eval docker run --name $GITLAB_RUNNER_PROJECT_RUNNER_NAME ${DOCKER_RUN_EXTRAS} \
   --url "http://${DOCKER_HOST_ADDRESS}:$GITLAB_PORT" \
   --registration-token $PROJECT_REGISTRATION_TOKEN \
   --ssh-user $ANKA_VM_USER \

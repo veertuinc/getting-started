66b0d1b1b46753ce79a48804244c7db95a9e62eb
Nathan Pierce <connarpierce@gmail.com>
2021-11-30 15:58:43 -0500

    allow overriding TEMPLATE name

--- create-vm-template.bash
+++ create-vm-template.bash
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SCRIPT_DIR
 . ./shared.bash
 # cleanup() {
-#   sudo anka delete --yes $TEMPLATE || true
+#   sudo anka delete --yes $TEMPLATE_NAME || true
 # }
 # trap cleanup ERR INT
 [[ -z $(command -v jq) ]] && echo "JQ is required. You can install it with: brew install jq" && exit 1
@@ -20,8 +20,8 @@ if [[ "$1" == "--no-anka-create" ]] || [[ -z $1 ]]; then # interactive installer
   cp $SCRIPT_DIR/.misc/download-macos-installer.py $TEMP_DIR/
   sudo ./download-macos-installer.py --raw --workdir $TEMP_DIR/
   INSTALL_IMAGE=$(basename $TEMP_DIR/Install_*.sparseimage)
-  TEMPLATE="$(echo $INSTALL_IMAGE | sed -n 's/.*macOS_\([0-9][0-9]\..*\)-.*/\1/p')"
-  [[ "${#TEMPLATE}" -eq 4 ]] && TEMPLATE="${TEMPLATE}.0" # bug fix for 2.5.3 and 11.6 named templates
+  TEMPLATE_NAME=${TEMPLATE_NAME:-"$(echo $INSTALL_IMAGE | sed -n 's/.*macOS_\([0-9][0-9]\..*\)-.*/\1/p')"}
+  [[ "${#TEMPLATE_NAME}" -eq 4 ]] && TEMPLATE_NAME="${TEMPLATE_NAME}.0" # bug fix for 2.5.3 and 11.6 named templates
   echo "]] Mounting $INSTALL_IMAGE to $MOUNT_DIR ..."
   sudo hdiutil attach $INSTALL_IMAGE -mountpoint $MOUNT_DIR
   INSTALL_APP=$(basename $MOUNT_DIR/Applications/Install*.app)
@@ -30,19 +30,19 @@ if [[ "$1" == "--no-anka-create" ]] || [[ -z $1 ]]; then # interactive installer
   sudo hdiutil detach $MOUNT_DIR -force
 else
   [[ "${1:0:1}" != "/" ]] && echo "Ensure you're using the absolute path to your install .app" && exit 1
-  TEMPLATE="$(echo $1 | sed -n 's/.*macOS \(.*\).app/\1/p' | sed 's/ /-/g')"
-  [[ -z $TEMPLATE ]] && echo "Did you specify the path to an macOS installer .app?" && exit 1
+  TEMPLATE_NAME=${TEMPLATE_NAME:-"$(echo $1 | sed -n 's/.*macOS \(.*\).app/\1/p' | sed 's/ /-/g')"}
+  [[ -z $TEMPLATE_NAME ]] && echo "Did you specify the path to an macOS installer .app?" && exit 1
   INSTALLER_LOCATION="$1"
 fi
 echo "]] Installer placed at $INSTALLER_LOCATION"
 if [[ "$1" != "--no-anka-create" ]]; then
   cd $HOME
   # Cleanup already existing Template
-  sudo anka delete --yes $TEMPLATE &>/dev/null || true
+  sudo anka delete --yes $TEMPLATE_NAME &>/dev/null || true
   # Create Base Template
-  echo "]] Creating $TEMPLATE using $INSTALLER_LOCATION ..."
-  sudo ANKA_CREATE_SUSPEND=0 anka create --disk-size 100G --app "$INSTALLER_LOCATION" $TEMPLATE
-  modify_uuid $TEMPLATE $ANKA_BASE_VM_TEMPLATE_UUID
+  echo "]] Creating $TEMPLATE_NAME using $INSTALLER_LOCATION ..."
+  sudo ANKA_CREATE_SUSPEND=0 anka create --disk-size 100G --app "$INSTALLER_LOCATION" $TEMPLATE_NAME
+  modify_uuid $TEMPLATE_NAME $ANKA_BASE_VM_TEMPLATE_NAME_UUID
   # Add Registry to CLI (if the registry was installed locally)
   FULL_URL="${URL_PROTOCOL}$CLOUD_REGISTRY_ADDRESS"
   if AWS_USER_DATA="$(curl -s --connect-timeout 3 http://169.254.169.254/latest/user-data 2>/dev/null)"; then
@@ -52,5 +52,5 @@ if [[ "$1" != "--no-anka-create" ]]; then
     sudo anka registry add $CLOUD_REGISTRY_REPO_NAME ${FULL_URL}:$CLOUD_REGISTRY_PORT
     sudo anka registry list-repos
   fi
-  $SCRIPT_DIR/create-vm-template-tags.bash $TEMPLATE
+  $SCRIPT_DIR/create-vm-template-tags.bash $TEMPLATE_NAME
 fi

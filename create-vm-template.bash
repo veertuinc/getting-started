62eb785b42ea346048b1b201eb8e63586b5cc6d4
Nathan Pierce <norsegaud@gmail.com>
2022-04-13 15:14:18 -0400

    retry for create to handle EBS volume bug

--- create-vm-template.bash
+++ create-vm-template.bash
@@ -15,7 +15,10 @@ if [[ "$1" != "--no-anka-create" ]]; then
   sudo anka delete --yes $TEMPLATE_NAME &>/dev/null || true
   # Create Base Template
   echo "]] Creating $TEMPLATE_NAME using $INSTALLER_LOCATION ..."
-  time sudo ANKA_CREATE_SUSPEND=0 anka create --disk-size 100G --app "$INSTALLER_LOCATION" $TEMPLATE_NAME
+
+  # Retry after an hour and a half just in case macos fails to install for some reason
+  retry 2 time sudo ANKA_CREATE_SUSPEND=0 anka create --disk-size 100G --app "$INSTALLER_LOCATION" $TEMPLATE_NAME
+
   modify_uuid $TEMPLATE_NAME $ANKA_BASE_VM_TEMPLATE_UUID
   # Add Registry to CLI (if the registry was installed locally)
   FULL_URL="${URL_PROTOCOL}$CLOUD_REGISTRY_ADDRESS"

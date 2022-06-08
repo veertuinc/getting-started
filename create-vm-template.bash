df67507947101ef261a02fbfe75122f499ec0ae1
Nathan Pierce <norsegaud@gmail.com>
2022-06-08 13:19:07 -0400

    fix for aws logic

--- create-vm-template.bash
+++ create-vm-template.bash
@@ -32,9 +32,11 @@ if [[ "$1" != "--no-anka-create" ]]; then
   # Add Registry to CLI (if the registry was installed locally)
   FULL_URL="${URL_PROTOCOL}$CLOUD_REGISTRY_ADDRESS"
   if [[ -z "$(sudo anka registry list-repos | grep $CLOUD_REGISTRY_REPO_NAME || true)" ]]; then
-    AWS_USER_DATA="$(curl -s --connect-timeout 3 http://169.254.169.254/latest/user-data 2>/dev/null || true)"
-    if ! echo "${AWS_USER_DATA}" | grep "404 - Not Found"; then
-      FULL_URL="$(echo "${AWS_USER_DATA}" | grep ANKA_CONTROLLER_ADDRESS | cut -d\" -f2 | rev | cut -d: -f2-99 | rev)"
+    if curl -s --connect-timeout 3 http://169.254.169.254/latest/user-data 2>/dev/null; then
+      AWS_USER_DATA="$(curl -s --connect-timeout 3 http://169.254.169.254/latest/user-data 2>/dev/null)"
+      if ! echo "${AWS_USER_DATA}" | grep "404 - Not Found"; then
+        FULL_URL="$(echo "${AWS_USER_DATA}" | grep ANKA_CONTROLLER_ADDRESS | cut -d\" -f2 | rev | cut -d: -f2-99 | rev)"
+      fi
     fi
     sudo anka registry add $CLOUD_REGISTRY_REPO_NAME ${FULL_URL}:$CLOUD_REGISTRY_PORT
     sudo anka registry list-repos

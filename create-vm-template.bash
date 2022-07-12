dac6fe0eee172ff2c2a049e786b0486493d13ae6
Nathan Pierce <norsegaud@gmail.com>
2022-07-12 19:16:13 -0400

    create-vm-template m1 support

--- create-vm-template.bash
+++ create-vm-template.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -exo pipefail
 echo "]] Starting Anka VM Creation"
-[[ "$(arch)" == "arm64" ]] && echo "Anka 3.0 (ARM) is currently not supported" && exit 1
 MACOS_VERSION=${MACOS_VERSION:-"${1}"}
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SCRIPT_DIR
@@ -11,6 +10,18 @@ cd $SCRIPT_DIR
 TEMPLATE_NAME="${MACOS_VERSION}"
 INSTALLER_LOCATION="${INSTALL_MACOS_DIR}/${PREFIX_FOR_INSTALLERS}${MACOS_VERSION}${EXTENSION}"
 if [[ "$1" != "--no-anka-create" ]]; then
+  # Add Registry to CLI (if the registry was installed locally)
+  FULL_URL="${URL_PROTOCOL}$CLOUD_REGISTRY_ADDRESS"
+  if [[ -z "$(sudo anka registry list-repos | grep $CLOUD_REGISTRY_REPO_NAME || true)" ]]; then
+    if curl -s --connect-timeout 3 http://169.254.169.254/latest/user-data 2>/dev/null; then
+      AWS_USER_DATA="$(curl -s --connect-timeout 3 http://169.254.169.254/latest/user-data 2>/dev/null)"
+      if ! echo "${AWS_USER_DATA}" | grep "404 - Not Found"; then
+        FULL_URL="$(echo "${AWS_USER_DATA}" | grep ANKA_CONTROLLER_ADDRESS | cut -d\" -f2 | rev | cut -d: -f2-99 | rev)"
+      fi
+    fi
+    sudo anka registry add $CLOUD_REGISTRY_REPO_NAME ${FULL_URL}:$CLOUD_REGISTRY_PORT
+    sudo anka registry list-repos
+  fi
   cd $HOME
   # Cleanup already existing Template
   sudo anka delete --yes $TEMPLATE_NAME &>/dev/null || true
@@ -27,6 +38,13 @@ if [[ "$1" != "--no-anka-create" ]]; then
     sudo anka delete --yes "$TEMPLATE_NAME" || true
   done
   [ $NEXT_WAIT_TIME -lt ${RETRIES} ] || exit 5
+  [[ "$(arch)" == "arm64" ]] && ANKA_BASE_VM_TEMPLATE_UUID="${ANKA_BASE_VM_TEMPLATE_UUID_APPLE}" || ANKA_BASE_VM_TEMPLATE_UUID="${ANKA_BASE_VM_TEMPLATE_UUID_INTEL}"
   modify_uuid $TEMPLATE_NAME $ANKA_BASE_VM_TEMPLATE_UUID
+  if [[ "$(arch)" == "arm64" ]]; then
+    echo "At the moment the automated macOS installation process is not possible for Anka 3/Apple processors. You need to manually start the VM with anka start -uv {VMNAME} and finish the installation. \
+      Don't forget to install the Anka addons! See https://docs.veertu.com/anka/apple/getting-started/creating-your-first-vm/#3-start-the-vm-and-finish-the-macos-install for more information."
+    echo "Once you're done, stop the VM and run $SCRIPT_DIR/create-vm-template-tags.bash $TEMPLATE_NAME ${TAG_FLAGS}"
+    exit
+  fi
   $SCRIPT_DIR/create-vm-template-tags.bash $TEMPLATE_NAME ${TAG_FLAGS}
 fi

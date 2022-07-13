075d3b004e6da62fcb9d94a23b64bd9f93db08ad
Nathan Pierce <norsegaud@gmail.com>
2022-07-12 20:10:49 -0400

    m1 support

--- shared.bash
+++ shared.bash
@@ -2,7 +2,7 @@
 
 STORAGE_LOCATION=${STORAGE_LOCATION:-"/tmp"}
 URL_PROTOCOL=${URL_PROTOCOL:-"http://"}
-[[ "$(arch)" == "arm64" ]] && SUDO="" || SUDO="${SUDO} anka" # Can't open the anka viewer to install macos and addons as ${SUDO} anka.
+[[ "$(arch)" == "arm64" ]] && SUDO="" || SUDO="sudo" # Can't open the anka viewer to install macos and addons as ${SUDO} anka.
 
 if [[ "$(uname)" == "Darwin" ]]; then
   if tty -s; then # Disable if the shell isn't interactive (avoids: tput: No value for $TERM and no -T specified)

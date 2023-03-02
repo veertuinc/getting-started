d288d70ba71ba70a27b35421e40207a8598b2fdb
Nathan Pierce <norsegaud@gmail.com>
2023-03-02 14:39:04 -0500

    upgrade gitlab version

--- GITLAB/install-gitlab-on-docker.bash
+++ GITLAB/install-gitlab-on-docker.bash
@@ -11,7 +11,7 @@ docker rm $GITLAB_RUNNER_PROJECT_RUNNER_NAME || true
 execute-docker-compose down || true
 docker stop $GITLAB_DOCKER_CONTAINER_NAME &>/dev/null || true
 docker rm $GITLAB_DOCKER_CONTAINER_NAME &>/dev/null || true
-[[ -d $GITLAB_DOCKER_DATA_DIR ]] && sudo rm -rf $GITLAB_DOCKER_DATA_DIR
+[[ -d "${GITLAB_DOCKER_DATA_DIR}" ]] && sudo rm -rf "${GITLAB_DOCKER_DATA_DIR}"
 rm -rf docker-compose.yml
 if [[ $1 != "--uninstall" ]]; then
   EXTERNAL_URL=${EXTERNAL_URL:-"${URL_PROTOCOL}$GITLAB_DOCKER_CONTAINER_NAME:$GITLAB_PORT"}
@@ -25,6 +25,7 @@ version: '3.7'
 services:
   $GITLAB_DOCKER_CONTAINER_NAME:
     container_name: $GITLAB_DOCKER_CONTAINER_NAME
+    hostname: $GITLAB_DOCKER_CONTAINER_NAME
     image: gitlab/gitlab-$GITLAB_RELEASE_TYPE:$GITLAB_DOCKER_TAG_VERSION
     restart: always
     ports:
@@ -36,14 +37,14 @@ services:
       - $GITLAB_DOCKER_DATA_DIR/data:/var/opt/gitlab
     environment:
       GITLAB_OMNIBUS_CONFIG: |
-          external_url '$EXTERNAL_URL'
-          nginx['listen_port'] = $GITLAB_PORT
-          gitlab_rails['gitlab_ssh_host'] = '$GITLAB_DOCKER_CONTAINER_NAME'
-          gitlab_rails['gitlab_shell_ssh_port'] = 2244
-          gitlab_rails['initial_root_password'] = "$GITLAB_ROOT_PASSWORD"
-          gitlab_rails['signin_enabled'] = false
-          postgresql['max_files_per_process'] = 25
+        external_url "$EXTERNAL_URL"
+        nginx['listen_port'] = $GITLAB_PORT
+        postgresql['max_files_per_process'] = 25
+        gitlab_rails['gitlab_ssh_host'] = "$GITLAB_DOCKER_CONTAINER_NAME"
+        gitlab_rails['gitlab_shell_ssh_port'] = 2244
+    shm_size: '256m'
 BLOCK
+# gitlab_rails['signin_enabled'] = false
 if [[ "$(uname)" == "Linux" ]]; then
 cat >> docker-compose.yml <<BLOCK
     extra_hosts:
@@ -57,6 +58,10 @@ fi
     docker logs --tail 50 $GITLAB_DOCKER_CONTAINER_NAME 2>&1
     sleep 60
   done
+
+  # Set proper root password
+  docker exec -i anka.gitlab bash -c "gitlab-rails runner \"user = User.find_by_username('root'); user.password = \\\"${GITLAB_ROOT_PASSWORD}\\\"; user.password_confirmation = \\\"${GITLAB_ROOT_PASSWORD}\\\"; user.save!\""
+
   # Create project
   ## API auth
   # GITLAB_ACCESS_TOKEN=$(curl -s --request POST --data "grant_type=password&username=root&password=$GITLAB_ROOT_PASSWORD" http://$GITLAB_DOCKER_CONTAINER_NAME:$GITLAB_PORT/oauth/token | jq -r '.access_token')

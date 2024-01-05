883f578203ec80b8e1fd561cc205923c9c2269f8
Nathan Pierce <norsegaud@gmail.com>
2024-01-05 15:57:54 -0500

    quick fix

--- AWS/prepare-anka-node.bash
+++ AWS/prepare-anka-node.bash
@@ -125,10 +125,10 @@ fi
 
 # Create EC2 instance for Anka Node
 if [[ "${INSTANCE_ID}" == null ]]; then
-while [[ "$(aws_execute -r -s "ec2 describe-hosts --filter \"Name=tag:purpose,Values=${AWS_NONUNIQUE_LABEL}\"" | jq -r '.Hosts[0].State')" != 'available' ]]; do
-  echo "Dedicated Host still not available (this can take a while)..."
-  sleep 60
-done
+  while [[ "$(aws_execute -r -s "ec2 describe-hosts --filter \"Name=tag:purpose,Values=${AWS_NONUNIQUE_LABEL}\"" | jq -r '.Hosts[0].State')" != 'available' ]]; do
+    echo "Dedicated Host still not available (this can take a while)..."
+    sleep 60
+  done
   # Fix An error occurred (InvalidHostState) when calling the RunInstances operation: Dedicated host h-XXX is in an invalid state for launching instances.
   sleep 120
   while [[ "$(aws_execute -r -s "ec2 describe-hosts --host-ids \"${DEDICATED_HOST_ID}\"" | jq -r '.Hosts[0].AvailableCapacity.AvailableInstanceCapacity[0].AvailableCapacity')" != "1" ]]; do

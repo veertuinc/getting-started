063003d7e5179494129b648295401b439a41ef89
Nathan Pierce <norsegaud@gmail.com>
2024-06-04 16:32:20 -0500

    quick fix

--- PROMETHEUS/install-and-run-anka-prometheus-on-mac.bash
+++ PROMETHEUS/install-and-run-anka-prometheus-on-mac.bash
@@ -9,7 +9,7 @@ kill -15 $(pgrep "[a]nka-prometheus") 2>/dev/null || true
 if [[ $1 != "--uninstall" ]]; then
   echo "]] Downloading $PROMETHEUS_BINARY_NAME from github"
   pushd /tmp &>/dev/null
-  curl -L -O https://github.com/veertuinc/anka-prometheus/releases/download/v${PROMETHEUS_BINARY_VERSION}/${PROMETHEUS_BINARY_NAME}_v${PROMETHEUS_BINARY_VERSION}_darwin_amd64.zip
+  curl -L -O https://github.com/veertuinc/anka-prometheus-exporter/releases/download/v${PROMETHEUS_BINARY_VERSION}/${PROMETHEUS_BINARY_NAME}_v${PROMETHEUS_BINARY_VERSION}_darwin_amd64.zip
   unzip ${PROMETHEUS_BINARY_NAME}_v${PROMETHEUS_BINARY_VERSION}_darwin_amd64.zip
   chmod +x /tmp/$PROMETHEUS_BINARY_NAME
   echo "]] Running /tmp/$PROMETHEUS_BINARY_NAME --controller-address ${URL_PROTOCOL}$CLOUD_CONTROLLER_ADDRESS:${CLOUD_CONTROLLER_PORT} and backgrounding the process"
